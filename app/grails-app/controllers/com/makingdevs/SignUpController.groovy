dac14a516cc68dd80fab1f50d26b3c3f273644f1
neodevelop <neodevelop@gmail.com>
2013-07-31 11:26:14 -0500

    coding #6 redirect if logged in

--- app/grails-app/controllers/com/makingdevs/SignUpController.groovy
+++ app/grails-app/controllers/com/makingdevs/SignUpController.groovy
@@ -1,13 +1,20 @@
 package com.makingdevs
 
-import groovy.transform.ToString;
+import groovy.transform.ToString
+import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
 
 class SignUpController {
 
   def springSecurityService
   def signUpService
 
-  def index() { }
+  def index() {
+    def config = SpringSecurityUtils.securityConfig
+    if (springSecurityService.isLoggedIn()) {
+      redirect uri: config.successHandler.defaultTargetUrl
+      return
+    }
+  }
   def register(UserCommand userCommand) {
     User user = signUpService.registerUserWithUserCommand(userCommand)
     if(user.hasErrors()){
