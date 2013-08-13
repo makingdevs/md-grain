14e2e90c027f9764598ed1324fe8d9fe02c7b206
neodevelop <neodevelop@gmail.com>
2013-08-13 09:33:39 -0500

    coding #15 fixing taglib when no category code

--- app/grails-app/taglib/com/makingdevs/CourseTagLib.groovy
+++ app/grails-app/taglib/com/makingdevs/CourseTagLib.groovy
@@ -4,23 +4,25 @@ class CourseTagLib {
 
   def coursesPerCategory = { attrs, body ->
     def category = Category.findByCode(attrs.code)
-    out << """
-      <h3>
-        <i class='icon-th-list'></i>
-        ${category.name}
-      </h3>
-    """
-    out << "<ul>"
-    category.courses.each { course ->
-      if(!(attrs.currentCourse.toLong() == course.id)){
-        out << """
-          <li>
-            <a href='#'>${course.name}</a>
-          </li>
-        """  
+    if(category){
+      out << """
+        <h3>
+          <i class='icon-th-list'></i>
+          ${category.name}
+        </h3>
+      """
+      out << "<ul>"
+      category.courses.each { course ->
+        if(!(attrs.currentCourse.toLong() == course.id)){
+          out << """
+            <li>
+              <a href='#'>${course.name}</a>
+            </li>
+          """  
+        }
       }
+      out << "</ul>"
     }
-    out << "</ul>"
   }
 
   def scheduledCourses = { attrs, body ->
