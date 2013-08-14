3131e68438e588abb1ae1bf1a854b859e4ad9c5e
neodevelop <neodevelop@gmail.com>
2013-08-14 16:37:49 -0500

    testing #15 adding new test case and implementing, where the user already is registered

--- app/grails-app/services/com/makingdevs/RegistrationService.groovy
+++ app/grails-app/services/com/makingdevs/RegistrationService.groovy
@@ -5,9 +5,12 @@ class RegistrationService {
   Registration addUserToScheduledCourse(String username, Long scheduledCourseId) {
     def user = User.findByUsername(username)
     def scheduledCourse = ScheduledCourse.get(scheduledCourseId)
-    def registration = new Registration(user:user)
-    scheduledCourse.addToRegistrations(registration).save()
-    registration.save()
+    def registration = Registration.findByUserAndScheduledCourse(user,scheduledCourse)
+    if(!registration){
+      registration = new Registration(user:user)
+      scheduledCourse.addToRegistrations(registration).save()
+      registration.save()  
+    }
     registration
   }
 }
--- app/test/unit/com/makingdevs/RegistrationServiceSpec.groovy
+++ app/test/unit/com/makingdevs/RegistrationServiceSpec.groovy
@@ -7,7 +7,6 @@ import grails.test.mixin.domain.DomainClassUnitTestMixin
 import grails.plugins.springsecurity.SpringSecurityService
 
 @TestFor(RegistrationService)
-//@TestMixin(DomainClassUnitTestMixin)
 @Mock([ScheduledCourse,User,Registration])
 class RegistrationServiceSpec extends Specification {
 
@@ -18,7 +17,7 @@ class RegistrationServiceSpec extends Specification {
       mockDomain(User)
       mockDomain(ScheduledCourse)
       new User(username:"me@me.com").save(validate:false)
-      new ScheduledCourse(iregistrations:[]).save(validate:false)
+      new ScheduledCourse().save(validate:false)
     when:
       def registration = service.addUserToScheduledCourse("me@me.com", 1L)
     then:
@@ -26,4 +25,22 @@ class RegistrationServiceSpec extends Specification {
       registration.registrationStatus == RegistrationStatus.REGISTERED
       !registration.invoice
 	}
+
+  def "Adding a user to a scheduled course when already is added"(){
+    given: "An scheduled course and existing user"
+      User.metaClass.isDirty = { true }
+      User.metaClass.encodePassword = {"password"}
+      mockDomain(User)
+      mockDomain(ScheduledCourse)
+      def user = new User(username:"me@me.com").save(validate:false)
+      new ScheduledCourse().save(validate:false)
+      def registration = new Registration(user:user)
+      ScheduledCourse.get(1L).addToRegistrations(registration).save()
+      registration.save(validate:false)
+    when:
+      service.addUserToScheduledCourse("me@me.com", 1L)
+      def scheduledCourse = ScheduledCourse.get(1L)
+    then:
+      scheduledCourse.registrations.size() ==  1
+  }
 }
