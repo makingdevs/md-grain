b32c6e0ed8b72feb81c5212f511b3f1ca4a2977d
neodevelop <neodevelop@gmail.com>
2013-08-20 13:14:39 -0500

    coding #27 using perfil class

--- app/grails-app/services/com/makingdevs/SignUpService.groovy
+++ app/grails-app/services/com/makingdevs/SignUpService.groovy
@@ -1,18 +1,20 @@
 package com.makingdevs
 
+import com.makingdevs.*
+
 class SignUpService {
 
   def registerUserWithUserCommand(UserCommand userCommand) {
     User user = new User(userCommand.properties)
-    Profile profile = new Profile(userCommand.properties)
-    log.debug profile
-    user.profile = profile
+    Perfil perfil = new Perfil(userCommand.properties)
+    log.debug perfil
+    user.perfil = perfil
     user.enabled =  true
     if(user.validate()){
       user.save()
       Role roleStudent = Role.findByAuthority("ROLE_STUDENT")
       UserRole.create(user,roleStudent)
-      profile.save()
+      perfil.save()
       user.save()
     }
     user
