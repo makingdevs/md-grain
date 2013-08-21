786762b695584bb74e4f957309c0707a494b0b5d
neodevelop <neodevelop@gmail.com>
2013-08-21 11:11:39 -0500

    coding #23 adding perfil controller actions and templates

--- app/grails-app/controllers/com/makingdevs/PerfilController.groovy
+++ app/grails-app/controllers/com/makingdevs/PerfilController.groovy
@@ -2,5 +2,34 @@ package com.makingdevs
 
 class PerfilController {
 
-  def index() { }
+  def springSecurityService
+  def perfilService
+  def pagoService
+
+  def index() {
+    def usuarioActual = springSecurityService.currentUser
+    [usuarioActual : usuarioActual]
+  }
+
+  def actualizarPassword(UpdatePasswordCommand upc) {
+    if(upc.hasErrors()) {
+      render upc.errors
+      return 
+    }
+    perfilService.actualizarPasswordForUser(upc.nuevaContrasenia, springSecurityService.currentUser)
+    redirect controller:'perfil'
+  }
+}
+
+class UpdatePasswordCommand {
+
+  String nuevaContrasenia
+  String confirmaContrasenia
+
+  static constraints = {
+    nuevaContrasenia blank:false, nullable : false, minSize:6
+    confirmaContrasenia blank:false, nullable : false, minSize:6, validator: { value, obj -> 
+      value.size() == obj.nuevaContrasenia.size() && value == obj.nuevaContrasenia
+    }
+  }
 }
---
+++ app/grails-app/views/perfil/_header.gsp
@@ -0,0 +1,9 @@
+<div class="media">
+  <a class="pull-left" href="#">
+    <img src="${usuarioActual.perfil.avatar}" style="width: 64px; height: 64px;" alt="64x64" class="media-object img-polaroid" data-src="holder.js/64x64">
+  </a>
+  <div class="media-body">
+    <h4 class="media-heading">${usuarioActual.perfil.nombre} ${usuarioActual.perfil.apellidoPaterno} ${usuarioActual.perfil.apellidoMaterno}</h4>
+    <i class="icon-user"></i> ${usuarioActual.username}
+  </div>
+</div>
