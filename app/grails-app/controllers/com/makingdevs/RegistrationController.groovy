1ba49b5d49792c474a916663a96a487c9c17ebbd
neodevelop <neodevelop@gmail.com>
2013-08-21 14:41:03 -0500

    coding #19 redirecting  to myTraining controller

--- app/grails-app/controllers/com/makingdevs/RegistrationController.groovy
+++ app/grails-app/controllers/com/makingdevs/RegistrationController.groovy
@@ -11,5 +11,6 @@ class RegistrationController {
 
   def addMe(){
     registrationService.addUserToScheduledCourse(springSecurityService.currentUser, params.id.toLong())
+    redirect controller:"myTraining"
   }
 }
