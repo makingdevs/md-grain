c1860a210b9b6c25ccade926ce2077572af1d9b6
neodevelop <neodevelop@gmail.com>
2013-08-22 13:25:03 -0500

    coding #21 adding changeset

--- app/grails-app/migrations/adding-content-course.groovy
+++ app/grails-app/migrations/adding-content-course.groovy
@@ -2,9 +2,13 @@ databaseChangeLog = {
 
 	changeSet(author: "neodevelop (generated)", id: "1377193750656-1") {
 		addColumn(tableName: "course") {
-			column(name: "content", type: "varchar(255)") {
+			column(name: "content", type: "longtext") {
 				constraints(nullable: "false")
 			}
 		}
 	}
+
+  changeSet(author: "neodevelop (generated)", id: "1377193750656-1") {
+    sql("update course set content='content' where 1=1")
+  }
 }
