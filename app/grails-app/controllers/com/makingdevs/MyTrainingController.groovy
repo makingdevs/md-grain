36642e0f9b82abe76866a23b5e824b95ad587562
neodevelop <neodevelop@gmail.com>
2013-09-04 19:50:42 -0500

    coding #37 adding action to send mails

--- app/grails-app/controllers/com/makingdevs/MyTrainingController.groovy
+++ app/grails-app/controllers/com/makingdevs/MyTrainingController.groovy
@@ -3,10 +3,22 @@ package com.makingdevs
 class MyTrainingController {
 
   def springSecurityService
+  def mailService
 
   def index() {
     def usuarioActual = springSecurityService.currentUser
     def registrations = Registration.findAllByUser(usuarioActual,[fetch:[scheduledCourse:'join']])
     [registrations:registrations]
+  }
+
+  def sendPaymentInstructions(){
+    log.debug "OK"
+    mailService.sendMail {
+      to "juan@makingdevs.com"
+      from "info@makingdevs.com"
+      subject "Hello John"
+      body 'this is some text'
+    }
+    render "ok"
   }
 }
