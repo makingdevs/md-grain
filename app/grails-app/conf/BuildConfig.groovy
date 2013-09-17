7d137d176e7d8bd84f9abc4a5ba42e4716d9ffaa
felipe <fjuarezmurillo@synergyj.com>
2013-09-17 14:14:54 -0500

    coding #36 cambiando la versión del database migration

--- app/grails-app/conf/BuildConfig.groovy
+++ app/grails-app/conf/BuildConfig.groovy
@@ -12,6 +12,8 @@ grails.project.source.level = 1.6
 //  run: [maxMemory:1024, minMemory:64, debug:false, maxPerm:256]
 //]
 
+// grails.plugin.location.'Payable' = "../../payable"
+
 grails.project.dependency.resolution = {
   // inherit Grails' default dependencies
   inherits("global") {
@@ -57,7 +59,7 @@ grails.project.dependency.resolution = {
     }
 
     build ":tomcat:$grailsVersion"
-    runtime ":database-migration:1.3.5"
+    runtime ":database-migration:1.3.6"
     compile ':cache:1.0.1'
     compile ':profile:latest.integration'
   }
