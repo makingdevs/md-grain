4e061242c510c6927f6c47bcab415a668790b1b6
felipe <fjuarezmurillo@synergyj.com>
2013-09-18 12:57:17 -0500

    coding #34 adding services to retrieve payments

--- app/grails-app/controllers/com/makingdevs/PaymentController.groovy
+++ app/grails-app/controllers/com/makingdevs/PaymentController.groovy
@@ -2,8 +2,14 @@ package com.makingdevs
 
 class PaymentController {
 
+  def springSecurityService
+  def pagoService
+
   def myPayments() {
-    
+    def user = springSecurityService.currentUser
+    def payments = pagoService.estadoDeCuentaUsuario(user)
+
+    [payments:payments]
   }
 
 }
