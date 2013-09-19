7872ad94023400c9b3920682b4c1c288c7a425df
neodevelop <neodevelop@gmail.com>
2013-09-19 10:38:55 -0500

    coding #39 fixing schema for tables

--- app/grails-app/migrations/adding-constraints-to-esquema-de-pago.groovy
+++ app/grails-app/migrations/adding-constraints-to-esquema-de-pago.groovy
@@ -1,5 +1,24 @@
 databaseChangeLog = {
 
+  changeSet(author: "neodevelop (generated)", id: "1379554536322-0") {
+    sql('ALTER TABLE concepto ENGINE=InnoDB;')
+    sql('ALTER TABLE course_session ENGINE=InnoDB;')
+    sql('ALTER TABLE descuento ENGINE=InnoDB;')
+    sql('ALTER TABLE esquema_de_pago ENGINE=InnoDB;')
+    sql('ALTER TABLE esquema_de_pago_descuento ENGINE=InnoDB;')
+    sql('ALTER TABLE organizacion ENGINE=InnoDB;')
+    sql('ALTER TABLE organizacion_direccion ENGINE=InnoDB;')
+    sql('ALTER TABLE organizacion_telefono ENGINE=InnoDB;')
+    sql('ALTER TABLE pago ENGINE=InnoDB;')
+    sql('ALTER TABLE pago_descuento ENGINE=InnoDB;')
+    sql('ALTER TABLE payable ENGINE=InnoDB;')
+    sql('ALTER TABLE payable_pago ENGINE=InnoDB;')
+    sql('ALTER TABLE recargo ENGINE=InnoDB;')
+    sql('ALTER TABLE registration ENGINE=InnoDB;')
+    sql('ALTER TABLE s3asset ENGINE=InnoDB;')
+    sql('ALTER TABLE s3asset_options ENGINE=InnoDB;')
+  }  
+
   changeSet(author: "makingdevs (generated)", id: "1379554536322-1") {
     addColumn(tableName: "scheduled_course") {
       column(name: "esquema_de_pago_id", type: "bigint") {
