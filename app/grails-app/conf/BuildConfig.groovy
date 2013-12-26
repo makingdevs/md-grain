c1b3052ab4d529a2427d5aa150bde057b8b78e97
neodevelop <neodevelop@gmail.com>
2013-12-26 17:20:06 -0600

    coding #64 manually upgrading to grails 2.3.4

--- app/grails-app/conf/BuildConfig.groovy
+++ app/grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,6 @@
 import grails.util.Environment
 
-grails.servlet.version = "2.5"
+grails.servlet.version = "3.0" // Change depending on target container compliance (2.5 or 3.0)
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
@@ -8,11 +8,23 @@ grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
-//grails.project.fork = [
-//  run: [maxMemory:1024, minMemory:64, debug:false, maxPerm:256]
-//]
-
 //grails.plugin.location.'Payable' = "../../Payable"
+
+grails.project.fork = [
+    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+
+    // configure settings for the test-app JVM, uses the daemon by default
+    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+    // configure settings for the run-app JVM
+    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the run-war JVM
+    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the Console UI JVM
+    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+]
+
+grails.project.dependency.resolver = "maven" // or ivy
 
 grails.project.dependency.resolution = {
   // inherit Grails' default dependencies
@@ -33,7 +45,7 @@ grails.project.dependency.resolution = {
 
     mavenLocal()
     mavenCentral()
-    mavenRepo name: "MakingDevs", root: "http://makingdevs.com:8081/nexus/content/repositories/thirdparty"
+    mavenRepo("http://makingdevs.com:8081/nexus/content/repositories/thirdparty")
 
   }
 
@@ -45,7 +57,8 @@ grails.project.dependency.resolution = {
   }
 
   plugins {
-    runtime ":hibernate:$grailsVersion"
+    compile ":scaffolding:2.0.1"
+    runtime ":hibernate:3.6.10.4" // or ":hibernate4:4.1.11.4"
     runtime ":jquery:1.10.2.2"
     runtime ":resources:1.2"
     runtime ":cache-headers:1.1.5"
@@ -61,11 +74,11 @@ grails.project.dependency.resolution = {
       runtime ":yui-minify-resources:0.1.5"  
     }
 
-    build ":tomcat:$grailsVersion"
+    build ":tomcat:7.0.47"
     runtime ":database-migration:1.3.6"
     compile ':cache:1.0.1'
     compile ":mail:1.0.1"
     compile 'com.makingdevs:profile:0.1.6'
-    compile 'com.payable:payable:latest.integration'
+    compile 'com.payable:payable:0.2.9'
   }
 }
