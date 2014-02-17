ee54df6a3fa9f8521ea9eb2ffc728109ee147efa
Jorge Acosta <jorge@makingdevs.com>
2014-02-17 14:43:14 -0600

    coding #88 en processo con el metodo

--- app/grails-app/controllers/com/makingdevs/RegistrationController.groovy
+++ app/grails-app/controllers/com/makingdevs/RegistrationController.groovy
@@ -4,6 +4,7 @@ class RegistrationController {
 
   def registrationService
   def springSecurityService
+  def questionaryPerInstanceLinkService
 
   def index() {
     [scheduledCourse:ScheduledCourse.findById(params.id,[fetch:[courseSessions:'eager',course:'join']])]
@@ -16,5 +17,20 @@ class RegistrationController {
 
   def all(){
     [registrations:Registration.list(params)]
+  }
+
+  def firstEvaluate(){
+    def questionary = Questionary.findByCodeNameIlike("%${params.courseKey}-initial%")
+    def usuarioActual = springSecurityService.currentUser
+    if(usuarioActual){
+      def nombre="${params.courseKey}-initial"
+      def questionaryPerInstanceLink = questionaryPerInstanceLinkService.createQuestionaryPerInstance(usuarioActual,questionary.id)
+      redirect(action: "answerQuestionary", controller:"questionary", 
+        params: [id:questionaryPerInstanceLink.questionaryPerInstance.id,
+                idQL:questionaryPerInstanceLink.id,
+                url:params.url])
+    }else{
+      redirect(controller:"login")
+    }
   }
 }
