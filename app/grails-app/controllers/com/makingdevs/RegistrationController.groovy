a4777d61548b308b7772ba41e1996a1c1db0d2a7
Jorge Acosta <jorge@makingdevs.com>
2014-02-17 22:06:21 -0600

    coding #80 se termino la implementacion del metodo y sus validaciones

--- app/grails-app/controllers/com/makingdevs/RegistrationController.groovy
+++ app/grails-app/controllers/com/makingdevs/RegistrationController.groovy
@@ -20,15 +20,26 @@ class RegistrationController {
   }
 
   def firstEvaluate(){
-    def questionary = Questionary.findByCodeNameIlike("%${params.courseKey}-initial%")
+    def questionaryAcontestar = Questionary.findByCodeNameIlike("%${params.courseKey}-initial%")
     def usuarioActual = springSecurityService.currentUser
     if(usuarioActual){
-      def nombre="${params.courseKey}-initial"
-      def questionaryPerInstanceLink = questionaryPerInstanceLinkService.createQuestionaryPerInstance(usuarioActual,questionary.id)
-      redirect(action: "answerQuestionary", controller:"questionary", 
-        params: [id:questionaryPerInstanceLink.questionaryPerInstance.id,
-                idQL:questionaryPerInstanceLink.id,
+      if(questionaryAcontestar){
+        def nombre="${params.courseKey}-initial"
+        def questionaryPerInstanceLink=questionaryPerInstanceLinkService.findQuestionaryPerInstanceByCodeName(usuarioActual.id,nombre)
+        if(questionaryPerInstanceLink){
+          def numPreguntas=questionaryPerInstanceLink.questionaryPerInstance.questionary.questions.size()
+          redirect(controller:"evaluate", action:"evaluateQuestionary", params:[questionaryPerInstanceLink:questionaryPerInstanceLink.id,questionaryPerInstance:questionaryPerInstanceLink.questionaryPerInstance.id,
+            url:params.url,numPreguntas:numPreguntas])
+        }else{
+          def questionaryPerInstanceLinkNew = questionaryPerInstanceLinkService.createQuestionaryPerInstance(usuarioActual,questionaryAcontestar.id)
+            redirect(action: "answerQuestionary", controller:"questionary", 
+                params: [id:questionaryPerInstanceLinkNew.questionaryPerInstance.id,
+                idQL:questionaryPerInstanceLinkNew.id,
                 url:params.url])
+        }
+      }else{
+        redirect(uri: params.url-"md-grain/")
+      }
     }else{
       redirect(controller:"login")
     }
