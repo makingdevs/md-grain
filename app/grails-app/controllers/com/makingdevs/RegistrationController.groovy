7cb91967ff4940245162c0c82dba6c6ab9ac1c18
Jorge Acosta <jorge@makingdevs.com>
2014-02-20 13:22:16 -0600

    coding #92 se movio accion a una controller diferente

--- app/grails-app/controllers/com/makingdevs/RegistrationController.groovy
+++ app/grails-app/controllers/com/makingdevs/RegistrationController.groovy
@@ -4,7 +4,6 @@ class RegistrationController {
 
   def registrationService
   def springSecurityService
-  def questionaryPerInstanceLinkService
 
   def index() {
     [scheduledCourse:ScheduledCourse.findById(params.id,[fetch:[courseSessions:'eager',course:'join']])]
@@ -17,38 +16,5 @@ class RegistrationController {
 
   def all(){
     [registrations:Registration.list(params)]
-  }
-
-  def firstEvaluate(){
-    def questionaryAcontestar = Questionary.findByCodeNameIlike("%${params.courseKey}-initial%")
-    def usuarioActual = springSecurityService.currentUser
-    if(usuarioActual){
-      if(questionaryAcontestar){
-        def nombre="${params.courseKey}-initial"
-        def questionaryPerInstanceLink=questionaryPerInstanceLinkService.findQuestionaryPerInstanceByCodeName(usuarioActual.id,nombre)
-        if(questionaryPerInstanceLink){
-          if(questionaryPerInstanceLink.questionaryPerInstance.questionaryPerInstanceStatus==QuestionaryPerInstanceStatus.SIN_CONTESTAR){
-            redirect(action: "answerQuestionary", controller:"questionary", 
-                params: [id:questionaryPerInstanceLink.questionaryPerInstance.id,
-                idQL:questionaryPerInstanceLink.id,
-                url:params.url])
-          }else{
-            def numPreguntas=questionaryPerInstanceLink.questionaryPerInstance.questionary.questions.size()
-            redirect(controller:"evaluate", action:"evaluateQuestionary", params:[questionaryPerInstanceLink:questionaryPerInstanceLink.id,questionaryPerInstance:questionaryPerInstanceLink.questionaryPerInstance.id,
-              url:params.url,numPreguntas:numPreguntas])
-          }
-        }else{
-          def questionaryPerInstanceLinkNew = questionaryPerInstanceLinkService.createQuestionaryPerInstance(usuarioActual,questionaryAcontestar.id)
-            redirect(action: "answerQuestionary", controller:"questionary", 
-                params: [id:questionaryPerInstanceLinkNew.questionaryPerInstance.id,
-                idQL:questionaryPerInstanceLinkNew.id,
-                url:params.url])
-        }
-      }else{
-        redirect(uri: params.url-"md-grain/")
-      }
-    }else{
-      redirect(controller:"login")
-    }
   }
 }
