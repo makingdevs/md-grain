7cd1406effe4c740d7f984e677ef9a9f6063a818
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-07-22 20:51:08 -0500

    coding #115 updating profile view and creating controller to update the nickname

--- app/grails-app/controllers/com/makingdevs/PerfilController.groovy
+++ app/grails-app/controllers/com/makingdevs/PerfilController.groovy
@@ -8,4 +8,12 @@ class PerfilController {
     def usuarioActual = springSecurityService.currentUser
     [usuarioActual : usuarioActual]
   }
+
+  def update(){
+    def usuarioActual = springSecurityService.currentUser
+    usuarioActual.nickname = params.nickname
+    usuarioActual.perfil.publicProfile = (usuarioActual.nickname ?  true : false)
+    usuarioActual.save(flush:true)
+    render template:'/perfil/profileVisibility',model:[usuarioActual:usuarioActual]
+  } 
 }
