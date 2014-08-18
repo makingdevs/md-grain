682ea1640da682e1a7680da4c4bd4538bd2145d4
egjimenezg <egjimenezg@gmail.com>
2014-08-18 13:18:41 -0500

    coding #126 moving the logic from the controller to the service

--- app/grails-app/services/com/makingdevs/NotificationService.groovy
+++ app/grails-app/services/com/makingdevs/NotificationService.groovy
@@ -20,7 +20,18 @@ class NotificationService {
 
   }
 
-  def sendPaymentsInstructions(){
+  def findRegistrationAndSendPaymentsInstructions(registrationId){
+    def currentUser = springSecurityService.currentUser
+    Registration registration = Registration.findById(registrationId)
+    def totalAmount = registration.pagos*.cantidadDePago.sum(0) + registration.pagos*.recargosAcumulados.sum(0) - registration.pagos*.descuentoAplicable.sum(0)
+    
+    mailService.sendMail {
+      to currentUser.username
+      from "info@makingdevs.com"
+      subject "· Instrucciones de pago ·"
+      body( view:"/notification/paymentInfo", model:[course:registration.scheduledCourse.course,totalAPagar:totalAmount])
+    } 
 
+    registration
   }
 }
