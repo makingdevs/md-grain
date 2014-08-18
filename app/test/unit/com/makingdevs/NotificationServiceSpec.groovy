b33c77007529f701b8bd1c0e6c9a4101acef68e6
egjimenezg <egjimenezg@gmail.com>
2014-08-18 12:37:53 -0500

    coding #126 adding testing file

---
+++ app/test/unit/com/makingdevs/NotificationServiceSpec.groovy
@@ -0,0 +1,9 @@
+package com.makingdevs
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+@TestFor(NotificationService)
+class NotificationServiceSpec extends Specification {
+
+}
