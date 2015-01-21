7bb69df0e285f8651706bc9d8f2b1b57204a69eb
humberto <humberto2do@gmail.com>
2015-01-21 17:08:38 -0600

    coding #199 se agrega adjunto de archivo ics a la notificación

--- app/grails-app/services/com/makingdevs/NotificationService.groovy
+++ app/grails-app/services/com/makingdevs/NotificationService.groovy
@@ -6,16 +6,21 @@ import grails.transaction.Transactional
 class NotificationService {
 
   def mailService
+  def icalendarService
 
   def incribeStudentToCourse(username, scheduledCourseId) {
+    
     def user = User.findByUsername(username)
     def scheduledCourse = ScheduledCourse.get(scheduledCourseId)
+    def icsFile = icalendarService.createEvent(username, scheduledCourseId).getBytes("UTF-8")
 
     mailService.sendMail {
+      multipart true
       to user.username
       from "info@makingdevs.com"
       subject "Registro en ${scheduledCourse?.course?.name}"
       body( view:"/notification/inscriptionCourse", model:[scheduledCourse:scheduledCourse,nombre:user.perfil?.nombre])
+      attachBytes "register.ics", "text/calendar", icsFile
     }
 
   }
