7b3ddb9e7b3fdc25630bc7350ff8d4d0aa092490
Humberto <humberto2do@gmail.com>
2015-01-27 21:45:48 -0600

    coding #128 se crea servicio para manejar la creación de usuarios facebook

---
+++ app/grails-app/services/com/makingdevs/FacebookAuthService.groovy
@@ -0,0 +1,29 @@
+package com.makingdevs
+
+import grails.transaction.Transactional
+import com.the6hours.grails.springsecurity.facebook.FacebookAuthToken
+import org.springframework.social.facebook.api.Facebook
+import org.springframework.social.facebook.api.FacebookProfile
+import org.springframework.social.facebook.api.impl.FacebookTemplate
+
+@Transactional
+class FacebookAuthService {
+
+	def signUpService
+
+  def createAppUser(FacebookUser facebookUser, FacebookAuthToken token) {
+
+  	Facebook facebook = new FacebookTemplate(token.accessToken.accessToken)
+    FacebookProfile fbProfile = facebook.userOperations().userProfile
+  	def userCommand = new UserCommand()
+  	userCommand.nombre = fbProfile.firstName
+  	userCommand.apellidoPaterno = fbProfile.lastName
+  	userCommand.apellidoMaterno = ""
+  	userCommand.username = fbProfile.email
+  	userCommand.password = token.accessToken?.accessToken
+  	userCommand.nickname = "${System.currentTimeMillis()}"
+  	User user = signUpService.registerUserWithUserCommand(userCommand)
+  	user
+  }
+
+}
