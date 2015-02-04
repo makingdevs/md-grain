d5592d76ef7bb10ec4ee71b08e88e24b9295fc6b
humberto <humberto2do@gmail.com>
2015-02-04 11:56:35 -0600

    coding #130 se agrega action para manejo de registro de usuario twitter

--- app/grails-app/controllers/com/makingdevs/TwitterLoginController.groovy
+++ app/grails-app/controllers/com/makingdevs/TwitterLoginController.groovy
@@ -2,13 +2,55 @@ package com.makingdevs
 
 class TwitterLoginController {
 
-    def socialTwitterService
-    def index() { 
-      def authorizeUrl =  socialTwitterService.requestAccessTwitter(session)
-      redirect url: authorizeUrl
+  def socialTwitterService
+  def springSecurityService
+
+  def index() { 
+    def authorizeUrl =  socialTwitterService.requestAccessTwitter(session)
+    redirect url: authorizeUrl
+  }
+
+  def callback(){
+    def requestToken = session.requestToken
+    def oauthVerifier= params.oauth_verifier
+
+    if(!requestToken || !oauthVerifier){
+      redirect(action: "index")
+      return
     }
-    def callback(){
-      def profile = socialTwitterService.authenticateTwitterUser(session.requestToken, params.oauth_verifier)
-      profile
+
+    def profile = socialTwitterService.authenticateTwitterUser(requestToken, oauthVerifier)
+    def twitterUser = TwitterUser.findByTwitterId(profile.id)
+    if(twitterUser?.user){
+      twitterUser.token = profile.token
+      twitterUser.tokenSecret = profile.accessToken
+      twitterUser.save()
+      springSecurityService.reauthenticate twitterUser.user.username
+      redirect controller:'perfil'
+      return
     }
+    profile.oauth_verifier = oauthVerifier
+    profile
+  }
+
+  def register(){
+    
+    def email = params.username
+    def requestToken = session.requestToken
+    def oauthVerifier= params.oauth_verifier
+
+    if(!requestToken || !oauthVerifier || !email){
+      redirect(action: "index")
+      return
+    }
+
+    def twitterUser = socialTwitterService.createTwitterUser(email, requestToken, oauthVerifier)
+
+    if(!twitterUser.user){
+      redirect(action: "index")
+      return
+    }
+    springSecurityService.reauthenticate twitterUser.user.username
+    redirect controller:'perfil'
+  }
 }
