2eef27af00813f815a702620b552c9597cc66f6b
humberto <humberto2do@gmail.com>
2015-02-09 15:32:22 -0600

    coding #131 se agrega action de resgistro para github

--- app/grails-app/controllers/com/makingdevs/GithubLoginController.groovy
+++ app/grails-app/controllers/com/makingdevs/GithubLoginController.groovy
@@ -3,6 +3,7 @@ package com.makingdevs
 class GithubLoginController {
 
   def socialGithubService
+  def springSecurityService
   
   def index() { 
 
@@ -11,8 +12,39 @@ class GithubLoginController {
   }
 
   def callback(){
-    log.debug"callback de github ====== ${params}"
-    def profile = socialGithubService.authenticateGithubUser(params.code)
-    profile
+    def githubProfile = socialGithubService.authenticateGithubUser(params.code)
+    log.debug"githubProfile.githubId =============>>>>> ${githubProfile.githubId}"
+    def githubUser = GithubUser.findByGithubId(githubProfile.githubId)
+
+    if(!githubUser){
+      render view:'callback', model:githubProfile
+      return
+    }
+    if(githubUser.code != githubProfile.code){
+      redirect(action: 'index')
+      return
+    }
+    log.debug"first ---- aplicar login al usuario ********:))))"
+    log.debug" username ====== ${githubUser.user.username}"
+    springSecurityService.reauthenticate githubUser.user.username
+    redirect controller:'perfil'
+  }
+  def register(ProfileGithubCommand profileGithubCommand){
+    log.debug"parametros de ProfileGithubCommand ---> ${ProfileGithubCommand}"
+    if(profileGithubCommand.hasErrors()){
+      redirect(action: 'index')
+      return
+    }
+    def profile = socialGithubService.createGithubUser(profileGithubCommand)
+    def user = User.findByUsername(profile.email)
+    def githubUser = GithubUser.findByUser(user)
+    if(githubUser.code == profile.code){
+      log.debug" second ----- aplicar login al usuario ********:))))"
+      log.debug" username ====== ${githubUser.user.username}"
+      springSecurityService.reauthenticate githubUser.user.username
+      redirect controller:'perfil'
+      return
+    }
   }
 }
+
