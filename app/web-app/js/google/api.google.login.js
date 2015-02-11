46f942b46a57f212ed29f7ff12f454dcec0b1c69
Humberto <humberto2do@gmail.com>
2015-02-10 23:54:38 -0600

    coding #132 se crea callback javascript para manejar la respuesta de gmail

--- app/web-app/js/google/api.google.login.js
+++ app/web-app/js/google/api.google.login.js
@@ -1,5 +1,14 @@
-(function() {
-  var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-  po.src = 'https://apis.google.com/js/client:plusone.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-})();
+function signinCallback(authResult) {
+	console.log("entra a callback de google login.....");
+  if (authResult['access_token']) {
+    // Autorizado correctamente
+    // Oculta el botón de inicio de sesión ahora que el usuario está autorizado, por ejemplo:
+    document.getElementById('signinButton').setAttribute('style', 'display: none');
+  } else if (authResult['error']) {
+    // Se ha producido un error.
+    // Posibles códigos de error:
+    //   "access_denied": el usuario ha denegado el acceso a la aplicación.
+    //   "immediate_failed": no se ha podido dar acceso al usuario de forma automática.
+    // console.log('There was an error: ' + authResult['error']);
+  }
+}
