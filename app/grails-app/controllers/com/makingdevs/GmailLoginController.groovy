562fbd1eed2c5275ccca6fc2affc0dd0a1401e1a
humberto <humberto2do@gmail.com>
2015-02-13 16:31:54 -0600

    coding #132 se agrega redirect ajax para autenticación

--- app/grails-app/controllers/com/makingdevs/GmailLoginController.groovy
+++ app/grails-app/controllers/com/makingdevs/GmailLoginController.groovy
@@ -6,19 +6,12 @@ class GmailLoginController {
   def springSecurityService
 
   def index() {
-  	log.debug"entra en gmail login ::::::)))))))))))"
     def gmailUser = socialGmailService.getPlus(params.access_token)
-    log.debug"[1] step"
     if(!gmailUser){
     	'Error al crear usuario de gmail'
     	return
     }
-    log.debug"[2] step"
     springSecurityService.reauthenticate gmailUser.user.username
-    log.debug"[3] step"
-
-    render(contentType:"text/json") {
-      [url : createLink(controller:'profile')]
-    }
+    redirect controller:'login', action:'ajaxSuccess'
   }
 }
