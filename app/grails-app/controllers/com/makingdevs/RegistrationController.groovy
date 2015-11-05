9bda15ada313834e23a7818dd7fba69f2357d795
neodevelop <neodevelop@gmail.com>
2015-11-04 23:25:50 -0600

    Flushing the domain entity, wuack....

--- app/grails-app/controllers/com/makingdevs/RegistrationController.groovy
+++ app/grails-app/controllers/com/makingdevs/RegistrationController.groovy
@@ -12,12 +12,12 @@ class RegistrationController {
   def addMe(){
     def usuarioActual = springSecurityService.currentUser
     if (usuarioActual) {
-      registrationService.addUserToScheduledCourse(usuarioActual.username, params.id.toLong())
+      def registration = registrationService.addUserToScheduledCourse(usuarioActual.username, params.id.toLong())
       redirect controller:"myTraining", model:[scheduledCourseId:params.id.toLong()]
     }else{
       redirect(controller:"login")
     }
-    
+
   }
 
   def all(){
