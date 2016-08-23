3e607f875f281b08e046dcdc0a43b4d28adf22d7
temocpalma <temopalmag@gmail.com>
2016-08-23 12:31:56 -0500

    #136 Ajustando configuración de log

--- app/grails-app/conf/logback.groovy
+++ app/grails-app/conf/logback.groovy
@@ -2,18 +2,17 @@ import java.nio.charset.Charset
 
 def basePath = System.getenv("CATALINA_BASE") ?: "."
 
-appender('ROLLING',RollingFileAppender) {
+appender('FILE',FileAppender) {
+  file = "${basePath}/logs/md-grain.log"
+  append = true
   encoder(PatternLayoutEncoder){
     charset = Charset.forName('UTF-8')
     pattern = '%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n'
   }
-  rollingPolicy(TimeBasedRollingPolicy){
-    FileNamePattern = "${basePath}/logs/md-grain.log"
-  }
 }
 
-logger 'grails.app.controllers', DEBUG, ['ROLLING'], false
-logger 'grails.app.services', DEBUG, ['ROLLING'], false
+logger 'grails.app.controllers', DEBUG, ['FILE'], false
+logger 'grails.app.services', DEBUG, ['FILE'], false
 logger('org.springframework', WARN)
-root(WARN, ['ROLLING'])
+root(WARN, ['FILE'])
 
