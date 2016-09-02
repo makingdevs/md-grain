5bfd83a42a9da1150ba9661fa2e04f16893bab3c
temocpalma <temopalmag@gmail.com>
2016-09-02 15:23:08 -0500

    #136-B Ajustando rutas de los resources

--- app/grails-app/conf/ApplicationResources.groovy
+++ app/grails-app/conf/ApplicationResources.groovy
@@ -1,15 +1,15 @@
 modules = {
   application {
-    resource url:'js/application.js'
+    resource url:'assets/javascripts/application.js'
   }
 
   venera {
     dependsOn 'jquery'
-    resource url:'assets/less/theme_venera_blue.less',attrs:[rel: "stylesheet/less", type:'css']
-    resource url:'assets/js/bootstrap.js'
-    resource url:'assets/js/prettify.js'
-    resource url:'assets/js/lightbox.js'
-    resource url:'assets/js/main.js'
+    resource url:'assets/stylesheet/less/theme_venera_blue.less',attrs:[rel: "stylesheet/less", type:'css']
+    resource url:'assets/javascripts/bootstrap.js'
+    resource url:'assets/javascripts/prettify.js'
+    resource url:'assets/javascripts/lightbox.js'
+    resource url:'assets/javascripts/main.js'
     dependsOn 'googleLogin'
   }
 
@@ -19,52 +19,52 @@ modules = {
 
   wysihtml5 {
     dependsOn 'jquery'
-    resource url:'js/wysihtml5/img/glyphicons-halflings.png'
-    resource url:'js/wysihtml5/js/wysihtml5-0.3.0_rc2.min.js'
-    resource url:'js/wysihtml5/bootstrap-wysihtml5-0.0.2.css'
-    resource url:'js/wysihtml5/bootstrap-wysihtml5-0.0.2.min.js'
-    resource url:'js/wysihtml5/wysihtml-init.js'
+    resource url:'assets/javascripts/wysihtml5/img/glyphicons-halflings.png'
+    resource url:'assets/javascripts/wysihtml5/js/wysihtml5-0.3.0_rc2.min.js'
+    resource url:'assets/javascripts/wysihtml5/bootstrap-wysihtml5-0.0.2.css'
+    resource url:'assets/javascripts/wysihtml5/bootstrap-wysihtml5-0.0.2.min.js'
+    resource url:'assets/javascripts/wysihtml5/wysihtml-init.js'
   }
 
   telefono {
-    resource url:'js/telefono/index.js'
+    resource url:'assets/javascripts/telefono/index.js'
   }
-  
+
   buttonLoader {
-    resource url:'js/util/loader.js'
+    resource url:'assets/javascripts/util/loader.js'
   }
 
   upload {
-    resource url:'js/upload/css/basic.css'
-    resource url:'js/upload/css/dropzone.css'
-    resource url:'js/upload/dropzone.min.js'
+    resource url:'assets/javascripts/upload/css/basic.css'
+    resource url:'assets/javascripts/upload/css/dropzone.css'
+    resource url:'assets/javascripts/upload/dropzone.min.js'
 
   }
 
   colorPicker{
-    resource url:'css/colpick.css'
-    resource url:'js/colPick/colpick.js'
-    resource url:'js/colPick/colorPickFunction.js'
+    resource url:'assets/stylesheets/colpick.css'
+    resource url:'assets/javascripts/colPick/colpick.js'
+    resource url:'assets/javascripts/colPick/colorPickFunction.js'
   }
 
   perfil{
-    resource url:'css/perfil.css'
+    resource url:'assets/stylesheets/perfil.css'
   }
 
   fullCalendar{
-    resource url:'css/fullcalendar.min.css'
-    resource url:'js/fullcalendar/lib/moment.min.js'
-    resource url:'js/fullcalendar/lib/jquery.min.js'
-    resource url:'js/fullcalendar/fullcalendar.js'
-    resource url:'js/fullcalendar/lang-all.js'
+    resource url:'assets/stylesheets/fullcalendar.min.css'
+    resource url:'assets/javascript/fullcalendar/lib/moment.min.js'
+    resource url:'assets/javascripts/fullcalendar/lib/jquery.min.js'
+    resource url:'assets/javascripts/fullcalendar/fullcalendar.js'
+    resource url:'assets/javascripts/fullcalendar/lang-all.js'
   }
 
   sessionCalendar{
     dependsOn 'fullCalendar'
-    resource url:'js/sessionCalendar/session.calendar.js'
+    resource url:'assets/javascripts/sessionCalendar/session.calendar.js'
   }
 
   googleLogin{
-    resource url:'js/google/api.google.login.js'
+    resource url:'assets/javascripts/google/api.google.login.js'
   }
 }
