9e3d5e175e3d4ffc2e6d3bbbbde36dc72d6597fe
neodevelop <juan@makingdevs.com>
2016-10-11 11:22:35 -0500

    #136 quitando bean no necesario

--- app/grails-app/conf/spring/resources.groovy
+++ app/grails-app/conf/spring/resources.groovy
@@ -1,9 +1,3 @@
 // Place your Spring DSL code here
 beans = {
-
-  localeResolver(org.springframework.web.servlet.i18n.SessionLocaleResolver) {
-    defaultLocale = new Locale("es","ES_MX")
-    java.util.Locale.setDefault(defaultLocale)
-  }
-
 }
