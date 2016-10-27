910b38c845d8563d8069a0116b4087685271090f
neodevelop <juan@makingdevs.com>
2016-10-27 13:27:38 -0500

    #140 usando render para crear el JSOn que pinta los datos de calendario

--- app/grails-app/controllers/com/makingdevs/CalendarController.groovy
+++ app/grails-app/controllers/com/makingdevs/CalendarController.groovy
@@ -12,22 +12,20 @@ class CalendarController {
     TimeZone tz = TimeZone.getTimeZone("UTC")
     def courseSessionList = CourseSession.list(fetch:['eager':'scheduledCourse'])
     def scheduledCourseList = ScheduledCourse.list(fetch:['eager':'course'])
-    render(contentType: "application/json") {
-      scheduledCourseList.collect { sc ->
-        def i = 1
-        sc.courseSessions.sort().collect { cs ->
-          [
-            id:cs.id,
-            backgroundColor:'#'+cs.scheduledCourse.course.backgroundColor,
-            borderColor:'#'+cs.scheduledCourse.course.borderColor,
-            textColor:'#'+cs.scheduledCourse.course.fontColor,
-            title:cs.scheduledCourse.course.name + " - Sesión ${i++}",
-            start:df.format(cs.sessionStartTime),
-            end:df.format(cs.sessionEndTime),
-            url:createLink(controller:'course',action:'landingPage',id:sc.course.courseKey.toLowerCase())
-          ]
-        }
-      }.flatten()
-    }
+    render scheduledCourseList.collect { sc ->
+      def i = 1
+      sc.courseSessions.sort().collect { cs ->
+        [
+        id:cs.id,
+        backgroundColor:'#'+cs.scheduledCourse.course.backgroundColor,
+        borderColor:'#'+cs.scheduledCourse.course.borderColor,
+        textColor:'#'+cs.scheduledCourse.course.fontColor,
+        title:cs.scheduledCourse.course.name + " - Sesión ${i++}",
+        start:df.format(cs.sessionStartTime),
+        end:df.format(cs.sessionEndTime),
+        url:createLink(controller:'course',action:'landingPage',id:sc.course.courseKey.toLowerCase())
+        ]
+      }
+    }.flatten() as JSON
   }
 }
